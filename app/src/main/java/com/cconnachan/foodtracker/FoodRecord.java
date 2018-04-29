bb4d84d88d9a3dfb6f7fec63c430c4455b0973d4
Claire <cconnachan@gmail.com>
2018-04-29 13:22:21 +0100

    FoodRecord class written, passed first tests

--- app/src/main/java/com/cconnachan/foodtracker/FoodRecord.java
+++ app/src/main/java/com/cconnachan/foodtracker/FoodRecord.java
@@ -1,4 +1,22 @@
 package com.cconnachan.foodtracker;
 
+import java.util.ArrayList;
+
 public class FoodRecord {
+
+    private ArrayList<Food> loggedFood;
+
+    public FoodRecord(){
+        this.loggedFood = new ArrayList<Food>();
+    }
+
+    public ArrayList<Food> getLoggedFood() {
+        return loggedFood;
+    }
+
+    public void addFood(Food foodToAdd){
+        loggedFood.add(foodToAdd);
+    }
+
+
 }
--- app/src/test/java/com/cconnachan/foodtracker/FoodRecordTest.java
+++ app/src/test/java/com/cconnachan/foodtracker/FoodRecordTest.java
@@ -1,4 +1,31 @@
 package com.cconnachan.foodtracker;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class FoodRecordTest {
+
+    private FoodRecord recordedFood;
+    private Food chips;
+
+    @Before
+    public void setup(){
+        recordedFood = new FoodRecord();
+        chips = new Food("chips", "15/03/2018", MealType.DINNER);
+    }
+
+    @Test
+    public void canGetLoggedFood(){
+        assertEquals(0, recordedFood.getLoggedFood().size());
+    }
+
+    @Test
+    public void canAddFood(){
+        recordedFood.addFood(chips);
+        assertEquals(1, recordedFood.getLoggedFood().size());
+    }
 }
