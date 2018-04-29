aa5ccc20d5da12ae79d044cb95061e4dd75c48ef
Claire <cconnachan@gmail.com>
2018-04-29 15:50:10 +0100

    home activity page added text views and image

--- app/src/main/java/com/cconnachan/foodtracker/HomeActivity.java
+++ app/src/main/java/com/cconnachan/foodtracker/HomeActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private TextView menuTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,16 +18,16 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    menuTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_add:
-                    mTextMessage.setText(R.string.title_add_food);
+                    menuTextMessage.setText(R.string.title_add_food);
                     return true;
                 case R.id.navigation_foodlist:
-                    mTextMessage.setText(R.string.title_food_list);
+                    menuTextMessage.setText(R.string.title_food_list);
                     return true;
                 case R.id.navigation_selectdates:
-                    mTextMessage.setText(R.string.title_select_dates);
+                    menuTextMessage.setText(R.string.title_select_dates);
                     return true;
             }
             return false;
@@ -39,7 +39,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        menuTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
