e7a90f592f14453ddba52bc13511e0712f01f09f
Claire <cconnachan@gmail.com>
2018-04-29 13:04:20 +0100

    added unit tests to food class, cannot figure out how to assertEquals on dates though...

--- app/src/main/java/com/cconnachan/foodtracker/Food.java
+++ app/src/main/java/com/cconnachan/foodtracker/Food.java
@@ -1,4 +1,40 @@
 package com.cconnachan.foodtracker;
 
-public class Food {
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Food implements Serializable {
+
+    private String name;
+    private MealType mealType;
+    private Date date;
+//    private String dateToConvert;
+
+    public Food(String name, String dateToConvert, MealType mealType){
+        this.name = name;
+        try {
+            this.date = new SimpleDateFormat("dd/MM/yyyy", Locale.UK).parse(dateToConvert);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.mealType = mealType;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public MealType getMealType() {
+        return mealType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
 }
--- app/src/test/java/com/cconnachan/foodtracker/FoodTest.java
+++ app/src/test/java/com/cconnachan/foodtracker/FoodTest.java
@@ -1,4 +1,38 @@
 package com.cconnachan.foodtracker;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 public class FoodTest {
+
+    private Food chips;
+    private Food cucumber;
+
+    @Before
+    public void setup(){
+        chips = new Food("chips", "15/03/2018", MealType.DINNER);
+        cucumber = new Food("cucumber", "01/01/2018", MealType.SNACK);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("chips", chips.getName());
+    }
+
+    @Test
+    public void canGetMealType(){
+        assertEquals(MealType.DINNER, chips.getMealType());
+    }
+
+    @Test
+    public void canGetdate(){
+        assertEquals((Date) Thu Mar 15 00:00:00 GMT 2018, chips.getDate());
+    }
+
+
 }
